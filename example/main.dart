7372fa823a1416f38e80c42e0be3264701f6f302
Sach97 <sacha.arbonel@hotmail.fr>
2019-01-30 14:54:10 +0100

    performance comparison first implementation better

--- example/main.dart
+++ example/main.dart
@@ -18,17 +18,17 @@ class BlogSpider extends Spider {
   @override
   List<String> get start_urls => super.start_urls;
 
-  // Future<String> start_requests() async {
-  //   Response response;
-  //   response = await Request(start_urls);
-  //   return response.data.toString();
-  // }
-
-  Future<List<String>> start_requests() async {
-    List<String> responses = [];
+  Future<List<Response>> start_requests() async {
+    List<Response> responses = <Response>[];
     await for (Response response in Requests) {
-      responses.add(response.data.toString());
+      responses.add(response);
     }
+    return responses;
+  }
+
+  Future<List<Response>> start_requests2() async {
+    List<Response> responses;
+    responses = await Requests2();
     return responses;
   }
 }
@@ -36,7 +36,31 @@ class BlogSpider extends Spider {
 main() {
   BlogSpider spider = BlogSpider();
   spider.name = "myspider";
-  spider.start_urls = ["http://quotes.toscrape.com/page/1/"];
-  spider.start_requests().then((List<String> val) => print(val));
-  //spider.start_requests();
+  spider.start_urls = [
+    "http://quotes.toscrape.com/page/1/",
+    "http://quotes.toscrape.com/page/2/",
+    "http://quotes.toscrape.com/page/3/"
+  ];
+  Stopwatch stopw1 = new Stopwatch()..start();
+  spider
+      .start_requests()
+      .then((List<Response> val) => val.forEach((Response res) {
+            res.data.toString();
+          }));
+
+  var elapsed1 = stopw1.elapsed;
+  //print('start_requests1() executed in ${stopw1.elapsed}');
+  stopw1.stop();
+
+  Stopwatch stopw2 = new Stopwatch()..start();
+  spider
+      .start_requests2()
+      .then((List<Response> val) => val.forEach((Response r) {
+            //print(r.data.toString());
+            r.data.toString();
+          }));
+  var elapsed2 = stopw2.elapsed;
+  //print('start_requests2() executed in ${stopw2.elapsed}');
+  stopw2.stop();
+  print("requests2 $elapsed2, requests1 $elapsed1");
 }
